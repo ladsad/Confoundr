6e621563d852d7168fec265853ef6a665d7cda2c
ladsad <emailofshauryak@gmail.com>
2026-07-05 17:50:17 +0530

    fix: compatibility with latest rq version

--- api/worker.py
+++ api/worker.py
@@ -1,12 +1,10 @@
 import os
 from redis import Redis
-from rq import Worker, Queue, Connection
+from rq import Worker
 
 redis_url = os.getenv('REDIS_URL', 'redis://localhost:6379')
-
 conn = Redis.from_url(redis_url)
 
 if __name__ == '__main__':
-    with Connection(conn):
-        worker = Worker(['default'])
-        worker.work()
+    worker = Worker(['default'], connection=conn)
+    worker.work()
